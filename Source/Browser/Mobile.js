3945c725110532f1682af6358388e448e34e49e7
robernet <robernet@gmail.com>
2014-08-25 12:40:12 -0500

    Update Mobile.js
    Browser.Platform.ios & Browser.Platform.name are deprecated. Using Browser.ios & Browser.name instead.

--- Source/Browser/Mobile.js
+++ Source/Browser/Mobile.js
@@ -22,7 +22,7 @@ Browser.Device = {
 	name: 'other'
 };
 
-if (Browser.Platform.ios){
+if (Browser.ios){
 	var device = navigator.userAgent.toLowerCase().match(/(ip(ad|od|hone))/)[0];
 	
 	Browser.Device[device] = true;
@@ -32,6 +32,6 @@ if (Browser.Platform.ios){
 if (this.devicePixelRatio == 2)
 	Browser.hasHighResolution = true;
 
-Browser.isMobile = !['mac', 'linux', 'win'].contains(Browser.Platform.name);
+Browser.isMobile = !['mac', 'linux', 'win'].contains(Browser.name);
 
 }).call(this);
